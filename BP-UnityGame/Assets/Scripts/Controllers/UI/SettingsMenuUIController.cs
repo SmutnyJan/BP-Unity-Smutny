4a0bbfbf3f223c0b74d6845e0e6af9e4db7d60b8
Jan Smutný <smutnyjan2003@seznam.cz>
2025-01-26 18:14:47 +0100

    save audio volume not working

--- BP-UnityGame/Assets/Scripts/Controllers/UI/SettingsMenuUIController.cs
+++ BP-UnityGame/Assets/Scripts/Controllers/UI/SettingsMenuUIController.cs
@@ -5,6 +5,9 @@ public class SettingsMenuController : MonoBehaviour
 {
     public Slider SFXVolumeSlider;
     public Slider MusicVolumeSlider;
+    public Button SaveButton;
+    public Button BackButton;
+
 
     void Start()
     {
@@ -17,4 +20,26 @@ public class SettingsMenuController : MonoBehaviour
     {
         
     }
+
+    public void OnBackButtonPressed()
+    {
+        AudioManager.Instance.PlayClipByName("UI_Button_Click_1", AudioManager.Instance.AudioLibrary.UI, AudioManager.Instance.SFXAudioSource);
+        SceneLoaderManager.Instance.LoadScene(SceneLoaderManager.ActiveScene.MainMenu);
+    }
+
+    public void OnSaveButtonPressed()
+    {
+        SaveLoadManager.Instance.Save(SaveLoadManager.SaveType.Settings);
+    }
+    public void OnSFXVolumeSliderChanged()
+    {
+        AudioManager.Instance.MusicVolume = AudioManager.Instance.AudioMixer.ConvertToDecibelValue(MusicVolumeSlider.value);
+    }
+
+    public void OnMusicVolumeSliderChanged()
+    {
+
+        AudioManager.Instance.SFXVolume = AudioManager.Instance.AudioMixer.ConvertToDecibelValue(SFXVolumeSlider.value);
+    }
+
 }

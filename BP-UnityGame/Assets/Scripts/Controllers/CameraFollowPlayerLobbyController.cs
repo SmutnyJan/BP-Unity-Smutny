afe31ad6459845b7b1ce4d72b0cd7a72e9791db2
Jan Smutný <smutnyjan2003@seznam.cz>
2025-01-30 18:53:37 +0100

    lobby camera controll

--- BP-UnityGame/Assets/Scripts/Controllers/CameraFollowPlayerLobbyController.cs
+++ BP-UnityGame/Assets/Scripts/Controllers/CameraFollowPlayerLobbyController.cs
@@ -2,17 +2,25 @@ using UnityEngine;
 
 public class CameraFollowPlayerLobbyController : MonoBehaviour
 {
-    public Transform player;
+    public Transform player; // Odkaz na hráèe
+    public float minX, maxX; // Limity pro kameru na ose X
 
-    private float fixedY;
+    private float fixedY; // Pevná výška kamery
 
     void Start()
     {
-        fixedY = transform.position.y;
+        fixedY = transform.position.y; // Uložení pevné Y pozice
     }
 
     void LateUpdate()
     {
-        transform.position = new Vector3(player.position.x, fixedY, transform.position.z);
+        // Získání X pozice hráèe
+        float targetX = player.position.x;
+
+        // Omezíme X tak, aby bylo mezi minX a maxX
+        targetX = Mathf.Clamp(targetX, minX, maxX);
+
+        // Nastavení nové pozice kamery
+        transform.position = new Vector3(targetX, fixedY, transform.position.z);
     }
 }

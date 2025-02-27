c8b6f2b5c48d13493045237d386e1f26c249ee50
SmutnyJan <smutnyjan2003@seznam.cz>
2025-02-27 23:43:17 +0100

    moving platform

---
+++ BP-UnityGame/Assets/Scripts/Controllers/MovingPlatformController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class MovingPlatformController : MonoBehaviour
+{
+    public GameObject TargetDestination;
+    public float speed = 2f;
+
+    private Vector3 _startPosition;
+    private Vector3 _targetPosition;
+    private Vector3 _currentTarget;
+
+
+    void Start()
+    {
+        _startPosition = this.transform.position;
+        _targetPosition = TargetDestination.transform.position;
+        _currentTarget = _targetPosition;
+
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
+
+        if (transform.position == _currentTarget)
+        {
+            _currentTarget = (_currentTarget == _targetPosition) ? _startPosition : _targetPosition;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.SetParent(this.transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+
+}
